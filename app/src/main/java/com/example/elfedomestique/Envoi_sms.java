2248106cb64b05da5e39f69352f873f7ad1b16e2
Laura <lbk.laura@gmail.com>
2019-10-11 16:38:11 +0200

    Envoie de Message (non fonctionnel)
    L'envoi d'un message fait crash l'activity et donc n'envoie pas le message

---
+++ app/src/main/java/com/example/elfedomestique/Envoi_sms.java
@@ -0,0 +1,14 @@
+package com.example.elfedomestique;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Envoi_sms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_envoi_sms);
+    }
+}
